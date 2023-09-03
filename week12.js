7ae81357dccac8c0dbbfe2fcef585b6948117fa5
MessicaH <135277410+MessicaH@users.noreply.github.com>
2023-09-03 04:14:10 -0500

    updated styling and added .css file

--- week12.js
+++ week12.js
@@ -1,13 +1,13 @@
 // "Charclara" = Character Class and Race, "Charname" = Character Name
 
 class Group {
-    constructor(charname) {
-        this.charname = charname;
+    constructor(name) {
+        this.name = name;
         this.player = [];
     }
 
     addPlayer(charname,charclara) {
-        // interior element that is part of each new Group element added
+        // interior element that is part of each new group element added
         this.players.push(new Player (charname,charclara));
     }
 }
@@ -20,32 +20,32 @@ class Player {
 }
 
 class GroupService {
-    //static url = "https://64f42331932537f4051a219d.mockapi.io";
-    fetch();
+    static url = "https://64f42331932537f4051a219d.mockapi.io";
+    // fetch();
 
     static getAllGroups() {
         return $.get(this.url);
-        // return all Groups from MockAPI url
+        // return all groups from MockAPI url
     }
 
     static getGroup(id) {
         return $.get(this.url + `/${id}`);
-        // Read Groups available or listed (C *R* UD)
+        // Read groups available or listed (C *R* UD)
     }
 
-    static createGroup(Group) {
-        // Takes the "class Group" array created
-        return $.post(this.url, Group)
-        // Create new Group item entry (*C* RUD)
+    static createGroup(group) {
+        // Takes the "class group" array created
+        return $.post(this.url, group)
+        // Create new group item entry (*C* RUD)
     }
 
-    static updateGroup(Group) {
+    static updateGroup(group) {
         return $.ajax({
-            url: this.url + `/${Group._id}`,
-            // "._id" to update Group entry within database
+            url: this.url + `/${group._id}`,
+            // "._id" to update group entry within database
             dataType : "json",
             // what we should be sending or updating it as
-            data: JSON.stringify(Group),
+            data: JSON.stringify(group),
             // how to format the data being sent
             contentType: "application/JSON",
             type: "PUT",
@@ -65,12 +65,12 @@ class GroupService {
 // DOM Management area
 
 class DOMManager {
-    static Groups;
-    // all Groups within this class
+    static groups;
+    // all groups within this class
 
     static getAllGroups() {
-        // calls from GroupService method and inserts for DOM usage
-        GroupService.getAllGroups().then(Groups => this.render(Groups));
+        // calls from groupService method and inserts for DOM usage
+        GroupService.getAllGroups().then(groups => this.render(groups));
         // ".then" is Promise for when call completed
     }
 
@@ -78,36 +78,37 @@ class DOMManager {
         GroupService.deleteGroup(id)
             .then(() => {
                 return GroupService.getAllGroups();
-                // Once a Group is deleted successfully, we want to retrieve all other remaining Groups and re-render them
+                // Once a group is deleted successfully, we want to retrieve all other remaining groups and re-render them
             })
-            .then((Groups) => this.render(Groups));
+            .then((groups) => this.render(groups));
         }
     
     static addPlayer(id) {
-        for (let Group of this.Groups) {
-            if (Group._id == id) {
-                Group.Players.push(new Player($(`#${Group._id}-Player-charname`).val(), $(`#${Group._id}-Player-charclara`).val()));
-                GroupService.updateGroup(Group)
-                    // Taking new Group and Players to update arrays via API with new data
+        for (let group of this.groups) {
+            if (group._id == group) {
+                group.players.push(new Player($(`#${group._id}-player-charname`).val(), 
+                $(`#${group._id}-player-charclara`).val()));
+                GroupService.updategroup(group)
+                    // Taking new group and Players to update arrays via API with new data
                     .then(() => {
                         return GroupService.getAllGroups();
                     })
-                    .then((Groups) => this.render(Groups));
+                    .then((groups) => this.render(groups));
                 }
             }
         }
     
-    static deletePlayer(GroupId, PlayerId) {
-        for (let Group of this.Groups) {
-            if (Group._id == GroupId) {
-                for (let Player of Group.Players) {
-                    if (Player._id == PlayerId) {
-                        Group.Players.splice(Group.Players.indexOf(Player), 1);
-                        GroupService.updateGroup(Group)
+    static deletePlayer(groupId, playerId) {
+        for (let group of this.groups) {
+            if (group._id == groupId) {
+                for (let player of group.players) {
+                    if (player._id == playerId) {
+                        group.players.splice(group.players.indexOf(player), 1);
+                        GroupService.updateGroup(group)
                         .then(() => {
                             return GroupService.getAllGroups();
                         })
-                        .then((Groups) => this.render(Groups));
+                        .then((groups) => this.render(groups));
                     }
                 }
             }
@@ -119,50 +120,50 @@ class DOMManager {
             .then(() => {
                 return GroupService.getAllGroups();
             })
-            .then((Groups) => this.render(Groups));
+            .then((groups) => this.render(groups));
     }
 
-    static render(Groups) {
-        // Take a list of Groups and render to DOM (HTML) visually
-        this.Groups = Groups;
+    static render(groups) {
+        // Take a list of groups and render to DOM (HTML) visually
+        this.groups = groups;
         $("#app").empty();
         //Div for the app HTML to be rendered within, cleared out each time something is rendered
-        for (let Group of Groups) {
-            // Loop through each instance of Group added, rendering in Div for "app"
+        for (let group of groups) {
+            // Loop through each instance of group added, rendering in Div for "app"
             $('#app').prepend(
-                // Write HTML for each Group rendered
-                `<div id="${Group._id}" class="card">
+                // Write HTML for each group rendered
+                `<div id="${group._id}" class="card">
                     <div class="card-header">
-                        <h2>${Group.name}</h2>
-                        <button class="btn btn-danger" onclick="DOMManager.deleteGroup('${Group._id}')">Delete</button>
+                        <h2>${group.name}</h2>
+                        <button class="btn btn-delete" onclick="DOMManager.deletegroup('${group._id}')">Delete group</button>
                     </div>
                     <div class="card-body">
                         <div class="card">
                             <div class="row">
                                 <div class="col-sm">
-                                    <input type="text" id="${Group._id}-Player-charname" class="form-control" 
-                                    placeholder="Jackmerius Tacktheratrix, D'Glester Hardunkichud">
+                                    <input type="text" id="${group._id}-player-charname" class="form-control" 
+                                    placeholder="Jackmerius Tacktheratrix">
                                 </div>
                                 <div class="col-sm">
-                                    <input type="text" id="${Group._id}-Player-charclara" class="form-control" 
-                                    placeholder="Half-Elf Bard, Dwarf Barbarian, etc.">                                
+                                    <input type="text" id="${group._id}-player-charclara" class="form-control" 
+                                    placeholder="Dwarf Barbarian, etc.">                                
                                 </div>
                             </div>
-                            <button id="${Group._id}-new-Player" onclick="DOMManager.addPlayer('${Group._id}')" 
-                            class="btn btn-primary form-control">Add Player</button>
+                            <button id="${group._id}-new-player" onclick="DOMManager.addPlayer('${group._id}')" 
+                            class="btn btn-create form-control">Add Player<i class="bi bi-person-plus"></i></button>
                         </div>
                     </div>
                 </div>
                 <br>`
-                // Each Group created ^^, now to add each Player inside each Group
+                // Each group created ^^, now to add each Player inside each group
             );
             for (let player of group.players) {
                 $(`#${group._id}`).find('.card-body').append(
                     `<p>
                         <span id="name-${player._id}"><strong>Character Name: </strong> ${player.charname}</span>
                         <span id="name-${player._id}"><strong>Character Race & Class: </strong> ${player.charclara}</span>
-                        <button class="btn btn-danger" onclick="DOMManager.deletePlayer('${Group._id}','${player._id}')">
-                        Delete Player</button>
+                        <button class="btn btn-delete" onclick="DOMManager.deletePlayer('${group._id}','${player._id}')">
+                        Remove Player<i class="bi bi-person-dash"></i></button>
                     </p>
                     `
                 )
@@ -173,9 +174,9 @@ class DOMManager {
 }
 
 
-$('#create-new-Group').click(() => {
-    DOMManager.createGroup($('#new-Group-name').val());
-    $('#new-Group-name').val('');
+$('#create-new-group').click(() => {
+    DOMManager.createGroup($('#new-group-name').val());
+    $('#new-group-name').val('');
 })
 
 /// Code that will run for each action
